8e97bcc30b9d3ddc6267f16414a90e755fcd19c0
MarcosRaia <raiamarcos@gmail.com>
2022-04-07 18:02:12 -0300

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import FormComponent from './Components/Form/form.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import FormComponent from './Components/Form/form.jsx';
 import NavComponent from './Components/navbar/menu.jsx';
+import Footer from './Components/footer/footer.jsx';
+
 function App() {
 
   return <>
-    <NavComponent/>
+    <NavComponent />
     <FormComponent />
-    
+    <Footer />
   </>;
 
 }
--- src/Components/Form/form.jsx
+++ src/Components/Form/form.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Form, Stack, Container, } from 'react-bootstrap';
+import { Form, Stack, Container, Button} from 'react-bootstrap';
+
+
 function FormComponent() {
     return <>
 
-        <Container className='p-8 h-100'>
+        <Container className='p-8 h-100 mt-5'>
             <p className='text-center p-8'>Faça seu Login</p>
             <Stack gap={3}>
                 <div className="m-auto">
-                <Form.Control className='w-100 mb-2'size="lg" type="text" placeholder="Login" />
-                <Form.Control size="lg" type="text" placeholder="Senha" />
-                <Form.Check className='d-flex justify-content-center mt-2'name='lembrar de mim' label="Lembrar de mim" type='radio' />                    </div>
+                    <Form.Control className='w-100 mb-2' size="lg" type="text" placeholder="Login" />
+                    <Form.Control size="lg" type="text" placeholder="Senha" />
+                    <Form.Check className='d-flex justify-content-center mt-2 p-2' name='lembrar de mim' label="Lembrar de mim" type='radio' />
+                    <Button className="w-100 mb-2" variant="danger">Login</Button>
+                </div>
+
             </Stack>
         </Container>
 
